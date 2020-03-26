51f716869447836793919c95e2e6ee8691c117fa
Scott Rod <djmtype@gmail.com>
2020-03-26 15:27:33 -0400

    update

--- src/_includes/partials/global/service-worker.js
+++ src/_includes/partials/global/service-worker.js
@@ -8,7 +8,9 @@ const EXCLUDED_URLS = [
   'admin',
   '.netlify',
   'https://identity.netlify.com/v1/netlify-identity-widget.js',
-  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js'
+  'https://unpkg.com/netlify-cms@^2.9.3/dist/netlify-cms.js',
+  'thanks',
+  'informer'
 ];
 
 // URLS that we want to be cached when the worker is installed
